3bbe00037e97540cdd4eb8a51802b34a501fbefb
Daniel Olivares <danieloddl2@hotmail.com>
2016-04-02 22:39:37 -0600

    Add progressCircle directive.

--- dist/angular-progressbar.js
+++ dist/angular-progressbar.js
@@ -1,1 +1,21 @@
-angular.module("ngProgressbar");
+angular.module("ngProgressbar")
+.directive("progressCircle", function(){
+  return {
+    restrict: "AE",
+    scope: {
+      amount: "="
+    },
+    link: function(scope, element, attrs){
+      var circle = new ProgressBar.Circle(element[0], {
+        color: attrs.color || '#555',
+        strokeWidth: attrs.strokeWidth || 1,
+        fill: attrs.fill || '#aaa',
+        trailColor: attrs.trailColor || "#f4f4f4",
+        trailWidth: attrs.trailWidth || 0.8,
+        text: attrs.text || null
+      });
+      
+      circle.animate(attrs.amount/100);
+    }
+  }
+});
